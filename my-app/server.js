6a9349ce48d1e8522fc3c02423aa435a82281741
SheharBano404 <sheharbanotaufeeq@gmail.com>
2026-02-01 15:04:26 +0500

    updated

---
+++ my-app/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/api/trigger-pipeline', (req, res) => {
+
+    setTimeout(() => {
+        res.json({
+            id: "BUILD-" + Math.floor(Math.random() * 10000),
+            status: "Success",
+            steps: [
+                "⬇️ Fetching source code from GitHub...",
+                "📦 Installing dependencies (npm install)...",
+                "🐳 Building Docker Image...",
+                "✅ Running Unit Tests (Pass)...",
+                "🚀 Deploying to Production Server..."
+            ],
+            finalMessage: "Pipeline execution successful! App is live."
+        });
+    }, 1000);
+});
+
+app.listen(port, () => {
+    console.log(`DevOps Server running at http://localhost:${port}`);
+});
